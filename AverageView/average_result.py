504269e2dcea92c4a6b3b5442974e29a76002edf
Rico <ricorx7@gmail.com>
2019-08-05 00:47:14 -0700

    UPdate plotting

--- AverageView/average_result.py
+++ AverageView/average_result.py
@@ -2,6 +2,7 @@ import pandas as pd
 from rti_python.Post_Process.Average.AverageWaterColumn import AverageWaterColumn
 import numpy as np
 
+
 class AverageResult:
 
     def __init__(self):
@@ -17,23 +18,47 @@ class AverageResult:
         print("first time: " + str(avg_ens[AverageWaterColumn.INDEX_FIRST_TIME]))
         print("last time: " + str(avg_ens[AverageWaterColumn.INDEX_LAST_TIME]))
         print("Num Beams: " + str(avg_ens[AverageWaterColumn.INDEX_NUM_BEAM]))
+        print("Num Bins: " + str(avg_ens[AverageWaterColumn.INDEX_NUM_BINS]))
 
         if avg_ens[AverageWaterColumn.INDEX_NUM_BEAM] == 4:
-            #if self.df_earth_east.empty:
             # Create Dataframe
             df_earth = pd.DataFrame(avg_ens[AverageWaterColumn.INDEX_EARTH], columns=["East", "North", "Vertical", "Error"])
 
-            east_array = np.array(df_earth['East'].values.tolist())                      # Faster to convert to numpy array and replace BAD_VELOCITY
-            df_earth['East'] = np.where(east_array > 88, None, east_array).tolist()      # Replace BAD_VELOCITY with None
+            # Replace bad values
+            #east_array = np.array(df_earth['East'].values.tolist())                      # Faster to convert to numpy array and replace BAD_VELOCITY
+            #df_earth['East'] = np.where(east_array > 88, None, east_array).tolist()      # Replace BAD_VELOCITY with None
+            df_earth['East'] = self.replace_bad_val_with_none(df_earth['East'])
+            df_earth['North'] = self.replace_bad_val_with_none(df_earth['North'])
+            df_earth['Vertical'] = self.replace_bad_val_with_none(df_earth['Vertical'])
+            df_earth['Error'] = self.replace_bad_val_with_none(df_earth['Error'])
 
             # Convert the bin rows to bin columns
             df_earth = df_earth.transpose()
 
-            # East Velocities
-            east_series = df_earth.loc["East"]
+            # Bin names
+            bin_nums = []
+            for bin_num in range(avg_ens[AverageWaterColumn.INDEX_NUM_BINS]):
+                bin_nums.append(bin_num)
 
-            self.df_earth_east = self.df_earth_east.append(east_series, ignore_index=True)
+            # Set the new column names with the bin numbers
+            df_earth.columns = bin_nums
+            #df_earth.reindex(columns=sorted(df_earth.columns))
+
+            # Velocities
+            self.df_earth_east = self.df_earth_east.append(df_earth.loc["East"], ignore_index=True)
             self.df_earth_north = self.df_earth_north.append(df_earth.loc["North"], ignore_index=True)
             self.df_earth_vertical = self.df_earth_vertical.append(df_earth.loc["Vertical"], ignore_index=True)
             self.df_earth_error = self.df_earth_error.append(df_earth.loc["Error"], ignore_index=True)
 
+    def replace_bad_val_with_none(self, df):
+        """
+        Replace the BAD VALUE 88.888 with NONE.
+        This way the plots will not process the data as a high value.
+
+        Converting to a numpy was faster than doing it through the dataframe.
+
+        :param df: Data frame to replace values.
+        :return: Dataframe with no bad values.
+        """
+        df_as_array = np.array(df.values.tolist())                      # Faster to convert to numpy array and replace BAD_VELOCITY
+        return np.where(df_as_array > 88, None, df_as_array).tolist()   # Replace BAD_VELOCITY with None
--- AverageView/plot_data.py
+++ AverageView/plot_data.py
@@ -1,6 +1,8 @@
 from bokeh.io import show
 from bokeh.models import LinearColorMapper, BasicTicker, PrintfTickFormatter, ColorBar
 from bokeh.plotting import figure
+from bokeh.io import output_file, show
+from bokeh.models import ColumnDataSource
 import matplotlib.pyplot as plt
 from PyQt5 import QtCore
 
@@ -14,7 +16,8 @@ class PlotData(QtCore.QThread):
 
     def run(self):
         # Plot the East STD and Mean
-        self.plot_mpl_avg_east_std_mean(self.avg_result.df_earth_east)
+        #self.plot_mpl_avg_east_std_mean(self.avg_result.df_earth_east)
+        self.plot_bokeh_avg_east_heatmap(self.avg_result.df_earth_east)
 
     def plot_mpl_avg_east_std_mean(self, east_df):
         east_mean = east_df.mean()
@@ -25,3 +28,11 @@ class PlotData(QtCore.QThread):
         east_std.plot(kind='line', label='STD')
         plt.legend(loc='best')
         plt.show()
+
+    def plot_bokeh_avg_east_heatmap(self, east_df):
+
+        source = ColumnDataSource(east_df)
+
+        p = figure()
+        p.rect(source=source, title='East Velocity Average', stat=None)
+        show(p)
